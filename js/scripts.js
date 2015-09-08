ba118ae11d7c11078f8775239fcadc4e8cc624f1
Lisa Duschek & Lisa Folliard <student@epicodus.com>
2015-09-08 10:50:31 -0700

    return true if it is a triangle

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var triangleTracker = function(triangle){
+
+   var side1= 5;
+   var side2= 5;
+   var side3= 10;
+
+  if((side1+side2<=side3)) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+ describe('triangleTracker',function(){
+   it("returns true if it is a triangle",function() {
+     expect(triangleTracker(5,5,5)).to.equal(true);
+   });
+
+
+ });
